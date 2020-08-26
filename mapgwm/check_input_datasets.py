af9e9a2c7955a09c4bb1c965bf251ffa8eebd6bd
Howard W. Reeves <hwreeves@usgs.gov>
2020-08-26 12:50:07 -0400

    added script to check input datasets against those used in CI testing in case a change is made in the processing of input datasets before this step of the workflow. Rebased with upstream master.

--- mapgwm/check_input_datasets.py
+++ mapgwm/check_input_datasets.py
@@ -19,37 +19,11 @@ import os
 
 
 def get_header_length(sitefile, col0='SITE_BADGE'):
+    
     with open(sitefile) as src:
         for i, line in enumerate(src):
             if '#' not in str(line) and col0 in str(line):
                 return i
-
-def compare_lists(list1, name1, list2, name2):
-    """compare two lists and check for different or missing entries
-       print out missing or different entries in list 2 compared to list 1
-
-        Parameters
-        ----------
-        list1: list 
-        name1: str - name to be printed in comparison
-        list2: second list
-        name2: str - name to be printed in comparison
-
-        Returns
-        -------
-        none
-    """
-    no_match = [x for x in list2 if x not in list1]
-    missing = [x for x in list1 if x not in list2]
-    if len(no_match) > 0:
-        for x in no_match:
-            print('{0} is in {2} but not in {1}'.format(x, name1, name2))
-    elif len(missing) > 0:
-        for x in missing:
-            print('{0} is missing in {2} compared to {1}'.format(x, name1, name2))
-    else:
-        print('lists match: {0}, {1}'.format(name1, name2))
-
 
 
 def test_headobs(new_data_file, new_meta_file):
@@ -88,17 +62,13 @@ def test_headobs(new_data_file, new_meta_file):
     data_skiprows = get_header_length(new_meta_file)
     newmeta = pd.read_csv(new_meta_file, sep='\t', skiprows=data_skiprows)
 
-    compare_lists(testdata.columns.tolist(), 'Test Dataset', 
-                  newdata.columns.tolist(), 'New Dataset')
-
-    compare_lists(testmeta.columns.tolist(), 'Test Metafile', 
-                  newmeta.columns.tolist(), 'New Metafile')
+    assert (testdata.columns.tolist().sort() == newdata.columns.tolist().sort())
+    assert (testmeta.columns.tolist().sort() == newmeta.columns.tolist().sort())
+    print('headobs data files matched')
 
 
 if __name__ == '__main__':
-    # put path to your input file here, using same files
-    # as test directory as an example
-    
-    data_file = os.path.join('tests', 'data', 'headobs')
+    path = os.path.join('tests', 'data', 'headobs')
+    data_file = os.path.join(path, 'GW_monthly_stats_test.txt')
     metadata_file = os.path.join(path, 'GW_monthly_meta_test.txt')
     test_headobs(data_file, metadata_file)
