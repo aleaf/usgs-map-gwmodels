723e2d656b12dd00bd6ad59e3e63fae716c67e35
Howard W. Reeves <hwreeves@usgs.gov>
2020-08-28 08:57:15 -0400

    test paths were failing, replaced forward slashes with os.path.join items

--- mapgwm/tests/conftest.py
+++ mapgwm/tests/conftest.py
@@ -9,7 +9,7 @@ def project_root_path():
     two levels up from the location of this file.
     """
     filepath = os.path.split(os.path.abspath(__file__))[0]
-    return os.path.normpath(os.path.join(filepath, '../../'))
+    return os.path.normpath(os.path.join(filepath, '..', '..'))
 
 
 @pytest.fixture(scope="session")
@@ -17,14 +17,14 @@ def test_data_path(project_root_path):
     """Root folder for the project (with setup.py),
     two levels up from the location of this file.
     """
-    return os.path.join(project_root_path, 'mapgwm/tests/data')
+    return os.path.join(project_root_path, 'mapgwm', 'tests', 'data')
 
 
 @pytest.fixture(scope="session", autouse=True)
 def test_output_folder(project_root_path):
     """(Re)make an output folder for the tests
     at the begining of each test session."""
-    folder = project_root_path + '/mapgwm/tests/output'
+    folder = os.path.join(project_root_path, 'mapgwm', 'tests', 'output')
     reset = True
     if reset:
         if os.path.isdir(folder):
--- mapgwm/tests/test_headobs.py
+++ mapgwm/tests/test_headobs.py
@@ -4,8 +4,8 @@ from mapgwm.headobs import preprocess_headobs, get_data, get_active_area
 
 def test_preprocess_headobs(test_output_folder, test_data_path):
     # input files
-    data_file = os.path.join(test_data_path, 'headobs/GW_monthly_stats1990-01-01_2019-12-31.txt')
-    metadata_file = os.path.join(test_data_path, 'headobs/GW_monthly_meta1990-01-01_2019-12-31.txt')
+    data_file = os.path.join(test_data_path, 'headobs', 'GW_monthly_stats_test.txt')
+    metadata_file = os.path.join(test_data_path, 'headobs', 'GW_monthly_meta_test.txt')
 
     # output
     outputfile = os.path.join(test_output_folder, 'preprocessed_monthly_output.csv')
@@ -13,11 +13,11 @@ def test_preprocess_headobs(test_output_folder, test_data_path):
     start_date = '1998-04-01'
     
     # areas of interest within model to break out as separate observation groups
-    aoi = {'DeltaAOI': os.path.join(test_data_path, 'extents/CompositeHydrographArea.shp')
+    aoi = {'DeltaAOI': os.path.join(test_data_path, 'extents', 'CompositeHydrographArea.shp')
            }
     
     # area of observations to process (discard observations outside of this area)
-    active_area = get_active_area(os.path.join(test_data_path, 'extents/MERAS_Extent.shp'),
+    active_area = get_active_area(os.path.join(test_data_path, 'extents', 'MERAS_Extent.shp'),
                                   name_col='FORMATION',
                                   buffer=10000.)
 
