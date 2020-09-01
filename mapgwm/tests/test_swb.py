da7cb245b12e35bef0c85b4106a1d568f70911ca
Howard W. Reeves <hwreeves@usgs.gov>
2020-09-01 16:47:11 -0400

    Preproc swb (#2)
    reconciled the SWB preprocessing between the different model folders and made tests for swb and the check_input scripts. Need to add a method in the check_input_dataset script to check that the netcdf file has the same keys as those used for the test.

---
+++ mapgwm/tests/test_swb.py
@@ -0,0 +1,22 @@
+import os
+from mapgwm.swb import get_monthly_means
+from mfsetup import load_modelgrid
+
+
+def test_preprocess_swb(test_output_folder, test_data_path):
+    # input files
+    data_file = os.path.join(test_data_path, 'swb', 'cache_input_August2020.nc')
+
+    # output
+    outputfile = os.path.join(test_output_folder, 'swb_monthly_means.nc')
+
+    # get area of SWB to process
+    active_grid = load_modelgrid(os.path.join(test_data_path, 
+                                             'extents', 
+                                             'cache250_grid.json'))
+
+    # get the monthly means
+    get_monthly_means(data_file, outfile=outputfile,
+                      filter=active_grid.bounds, check_results=True)
+    assert os.path.exists(os.path.join(test_output_folder, 'swb_monthly_means.nc'))
+    
