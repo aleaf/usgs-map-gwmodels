1f8980df24dd4a93dfb5578b0a3a954847b358e0
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-20 14:07:05 +0000

    Update docs after building Travis build 59 of aleaf/usgs-map-gwmodels
    The docs were built from the branch 'master' against the commit
    8084ee9e84afa8a95ff4910c3e8966ed26da13dd.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/aleaf/usgs-map-gwmodels/jobs/402548412.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy --built-docs docs/build/html . --branch-whitelist master develop

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post41.dev0+gf1bf764',
+    VERSION: '0.post42.dev0+g8084ee9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
